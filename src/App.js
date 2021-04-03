a0e1caef80b9e82c5c09f34881eb2f7b7c731855
zinaelnahel <zinamahmoud1989@gmail.com>
2021-04-03 14:08:51 +0200

    creating the dictionary app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './original.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+ <img src={logo} className="App-logo"></img>
+    </div>
+  );
+}
+
+export default App;
