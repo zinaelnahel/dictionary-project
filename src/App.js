5d6173eccfa6d0bf63f273acd7ad1ee842d73840
zinaelnahel <zinamahmoud1989@gmail.com>
2021-04-04 16:35:11 +0200

    changes to dictionary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './original.png';
 import './App.css';
-
+import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
- <img src={logo} className="App-logo" alt="logo"></img>
+      <div className="container">
+        <header className="App-header">
+ <img src={logo} className="App-logo" alt="logo"></img></header>
+ <main> <Dictionary/></main>
+ <footer className="text-center">Coded by Zina El Nahel </footer>
  <p>lalalalalal</p>
-    </div>
-  );
-}
+      </div></div>
+  
+  )}
 
 export default App;
